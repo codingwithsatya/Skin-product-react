bf3f4d210c2e89452f754a588738c263b6d0545b
Satya Pramod <satyapramod5551@gmail.com>
2023-06-05 19:05:52 -0500

    Skin care product SPA using React

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import css from './Products.module.css';
+import Plane from '../../assets/plane.png';
+import { ProductsData } from "../../data/products";
+import { useAutoAnimate } from "@formkit/auto-animate/react"
+
+const Products = () => {
+
+    const [parent] = useAutoAnimate()
+
+    const [MenuProduct, setMenuProduct] = useState(ProductsData);
+
+    const filter = (type) => {
+        setMenuProduct(ProductsData.filter((product) => product.type === type));
+    }
+    return (
+        <div className={css.container}>
+            <img src={Plane} alt="" />
+            <h1>Our Feature Products</h1>
+
+            <div className={css.products}>
+                <ul className={css.menu}>
+                    <li onClick={() => setMenuProduct(ProductsData)}>All</li>
+                    <li onClick={() => filter("skin care")}>Skin Care</li>
+                    <li onClick={() => filter("conditioner")}>Conditioners</li>
+                    <li onClick={() => filter("foundation")}>Foundations</li>
+                </ul>
+
+                <div className={css.list} ref={parent}>
+                    {
+                        MenuProduct.map((product, i) => (
+                            <div className={css.product}>
+                                <div className="left-s">
+                                    <div className="name">
+                                        <span>{product.name}</span>
+                                        <span>{product.detail}</span>
+                                    </div>
+                                    <span>{product.price}$</span>
+                                    <div>Shop Now</div>
+                                </div>
+                                <img src={product.img} alt="" className='img-product' />
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Products
